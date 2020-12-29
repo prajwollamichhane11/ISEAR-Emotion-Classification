dd6ce01983c61565af19de8c9227ecb65e4c5cea
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2020-12-29 16:34:36 +0545

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is a part of training for AI Engineers workshop. This project will be our medium to develop prowess related to Software Engineering principles.',
+    author='Prajwol, Khusi',
+    license='MIT',
+)
