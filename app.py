cc57c2907f1bad91da9303a1154c505b2ddcb872
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2021-01-08 12:19:35 +0545

    Heroku Updates

---
+++ app.py
@@ -0,0 +1,38 @@
+import pickle
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+print(__name__)
+print(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    # Loading the vectorizer
+    cv = pickle.load(open("./models/vectorizer.pickle", "rb"))
+
+    # Loading the classifier
+    pickleFile = open("./models/nb_Classifier.pickle", "rb")
+    naiveBayesClf = pickle.load(pickleFile)
+
+    if request.method == "POST":
+        message = request.form["message"]
+        usrInp = [message]
+        vect = cv.transform(usrInp)
+        emoPrediction = naiveBayesClf.predict(vect)
+        print(emoPrediction)
+
+    return render_template("index.html", prediction=emoPrediction)
+
+
+if __name__ == "__main__":
+    app.run()
